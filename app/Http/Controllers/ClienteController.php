60fa57b893d4ea5a574b337d6c70e4ba47b61798
jorgeivanquintero <jorgeivan@ingenieros.com>
2022-02-24 00:06:26 -0500

    API completado

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -7,11 +7,48 @@ use App\Models\Cliente;
 
 class ClienteController extends Controller
 {
-    public $clientes;
+    //public $clientes;
     
     public function index(){
-        $this->clientes = Cliente::all();
-        return $this->clientes;
+             
+        $clientes = Cliente::all();
+        return \response($clientes);
    }
+   
+   public function store(Request $request){
+       
+       $request->validate([
+           'documento' => 'required',
+           'nombre' => 'required',
+           'telefono' => 'required',
+           'correo' => 'required',
+        ]);
+
+        $cliente = Cliente::create($request->all());
+        return \response($cliente);
+    }
+
+    public function show($id){
+               
+        $cliente = Cliente::findOrFail($id);
+        return \response($cliente);
+    }
+    
+    public function update(Request $request,$id){
+
+        $cliente = Cliente::findOrFail($id)
+            ->update($request->all());
+        
+        return \response("Cliente actualizado");
+    }
+    
+    public function destroy($id){
+
+        Cliente::destroy($id);
+        
+        return \response("Cliente eliminado");
+    }
+    
 }
+
 
